9b8711ad16985b2b7925ed3015a49f7f7691ad20
Kelvin <quocvm1@gmail.com>
2024-06-30 23:36:41 +0800

    remove unused files

--- example_factory_method/use_case_2/notification_system/target/debug/deps/notification_system-9cd54c3b3202c7cb.d
+++
@@ -1,5 +0,0 @@
-/Users/kelvin/KelvinZone/Rust/DesignPattern/example_factory_method/use_case_2/notification_system/target/debug/deps/libnotification_system-9cd54c3b3202c7cb.rmeta: src/main.rs
-
-/Users/kelvin/KelvinZone/Rust/DesignPattern/example_factory_method/use_case_2/notification_system/target/debug/deps/notification_system-9cd54c3b3202c7cb.d: src/main.rs
-
-src/main.rs:
