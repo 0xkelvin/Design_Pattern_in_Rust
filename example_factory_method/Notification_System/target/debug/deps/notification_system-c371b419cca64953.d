1bcc5b37f3936ac6e357527e1fde97c95db930d2
Kelvin <quocvm1@gmail.com>
2024-07-02 20:24:37 +0800

    remove unuse files

--- example_factory_method/Notification_System/target/debug/deps/notification_system-c371b419cca64953.d
+++
@@ -1,5 +0,0 @@
-/Users/kelvin/KelvinZone/Rust/DesignPattern/example_factory_method/Notification_System/target/debug/deps/libnotification_system-c371b419cca64953.rmeta: src/main.rs
-
-/Users/kelvin/KelvinZone/Rust/DesignPattern/example_factory_method/Notification_System/target/debug/deps/notification_system-c371b419cca64953.d: src/main.rs
-
-src/main.rs:
