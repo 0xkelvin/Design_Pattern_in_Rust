50230c11221ce2d4adf4a0bc88b3680c51ebe675
Kelvin <quocvm1@gmail.com>
2024-07-02 15:23:43 +0800

    remove unused files

--- example_factory_method/Payment_Processing_System/target/debug/deps/Payment_Processing_System-859b973a44d40176.d
+++
@@ -1,5 +0,0 @@
-/Users/kelvin/KelvinZone/Rust/DesignPattern/example_factory_method/Payment_Processing_System/target/debug/deps/libPayment_Processing_System-859b973a44d40176.rmeta: src/main.rs
-
-/Users/kelvin/KelvinZone/Rust/DesignPattern/example_factory_method/Payment_Processing_System/target/debug/deps/Payment_Processing_System-859b973a44d40176.d: src/main.rs
-
-src/main.rs:
